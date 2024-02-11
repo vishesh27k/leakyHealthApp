6781e9766fe193b29722ae5214f2d38f6364d7ad
vaibhavantil2 <88091026+vaibhavantil2@users.noreply.github.com>
2024-02-10 19:59:14 -0500

    Update MyApp.java

--- app/src/main/java/io/privado/privadohealthapp/MyApp.java
+++ app/src/main/java/io/privado/privadohealthapp/MyApp.java
@@ -1,26 +1,38 @@
 package io.privado.privadohealthapp;
 
 import android.app.Application;
-
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.amplitude.api.Amplitude;
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+import com.google.android.gms.ads.MobileAds;
 
 public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
 
+        // Initialize Facebook SDK
         FacebookSdk.setApplicationId(getString(R.string.facebook_application_id));
         FacebookSdk.setClientToken(getString(R.string.facebook_client_token));
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
+
+        // Initialize Firebase Analytics
         FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
+        // Initialize Amplitude
+        Amplitude.getInstance().initialize(this, getString(R.string.amplitude_api_key)).enableForegroundTracking(this);
 
-//        Criteo.getInstance().init(getApplicationContext(), getString(R.string.criteo_id));
+        // Initialize Mixpanel
+        MixpanelAPI mixpanel = MixpanelAPI.getInstance(this, getString(R.string.mixpanel_token));
 
-//        SnapPixel.init(this, getString(R.string.snap_pixel_id));
+        // Initialize Google AdMob
+        MobileAds.initialize(this, initializationStatus -> {});
 
+        // Commented-out Initializations
+        // Criteo.getInstance().init(getApplicationContext(), getString(R.string.criteo_id));
+        // SnapPixel.init(this, getString(R.string.snap_pixel_id));
     }
 }
